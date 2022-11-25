6787b42adef624d403cb0ee0a544ac7f96e6c1b7
JuanBiljoen <juangaljoen@gmail.com>
2022-11-25 19:30:36 +0200

    Official V0.10 release

--- src/components/About.js
+++ src/components/About.js
@@ -18,32 +18,32 @@ export const About = () => {
             })
         })
     }
- 
-      
-            
-                <button onClick={onButtonClick}></button>
+
+
+
+    <button onClick={onButtonClick}></button>
 
 
     return (
         <div className="about-container">
-        <div className="about" id="aboutme" xs={12} md={6} xl={7}>
-            <h2>About me</h2>
-            <p>
-                I am a fine jewellery designer looking to start a career in web development. I have a passion for creating beautiful looking products and a huge interest in creating attractive webpages and apps. I am looking for an oppurtunity to extend my knowledge and skills in web development. <br></br><br></br>
-                
-                <img className="background-about-image" src={"https://i.pinimg.com/564x/81/8f/4a/818f4a120483279ef50b769acf8188a6.jpg"} alt="astronaut" xs={12} md={6} xl={7}></img>
+            <div className="about" id="aboutme" xs={12} md={6} xl={7}>
+                <h2>About me</h2>
+                <p>
+                    I am a fine jewellery designer looking to start a career in web development. I have a passion for creating beautiful looking products and a huge interest in creating attractive webpages and apps. I am looking for an oppurtunity to extend my knowledge and skills in web development. <br></br><br></br>
 
-                In my spare time I'm either working on an old Vespa or taking my dog for a hike/fishing trip. I am very passionate about music, classic cars and motorbikes.</p>
+                    <img className="background-about-image" src={"https://i.pinimg.com/564x/81/8f/4a/818f4a120483279ef50b769acf8188a6.jpg"} alt="astronaut" xs={12} md={6} xl={7}></img>
+
+                    In my spare time I'm either working on an old Vespa or taking my dog for a hike/fishing trip. I am very passionate about music, classic cars and motorbikes.</p>
                 <h4>Click here to download my resume</h4>
-                
+
                 <button className="resume-button" onClick={onButtonClick}>Download resume</button>
 
-                    <div className="profileImg"><img role="presentation" className="me-pic" src={pic1} width="100" alt="profile pic" /></div>
-    
-                    
-                </div>
-            
+                <div className="profileImg"><img role="presentation" className="me-pic" src={pic1} width="100" alt="profile pic" /></div>
+
+
+            </div>
+
         </div>
-        
+
     )
 }  
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -6,9 +6,9 @@ export const ProjectCard = ({ title, description, imgUrl, link }) => {
   return (
     <Col size={4} sm={4} md={6} >
       <div className="proj-imgbx">
-      <a className="project-image" href={link} target="_blank" rel="noreferrer">
-        <img src={imgUrl} alt="Coding Beauty logo"></img>
-      </a>
+        <a className="project-image" href={link} target="_blank" rel="noreferrer">
+          <img src={imgUrl} alt="Coding Beauty logo"></img>
+        </a>
         {/* <img className="project-image" src={imgUrl} /> */}
         <div className="proj-txtx">
           <h4>{title}</h4>
